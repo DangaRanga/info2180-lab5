6121623fd9691af168a9d01986532bf3ba571ea3
Jason <jasonblue1626@gmail.com>
2020-11-19 18:51:35 -0500

    Completed functionality for searching for cities

--- world.js
+++ world.js
@@ -1,18 +1,34 @@
 // Ensure that the window loads before executing code
 window.addEventListener('load', function (){
-    let lookupBtn = document.getElementById('lookup');
+    const lookupBtn = document.getElementById('lookup');
+    const cityBtn = document.getElementById('lookup-cities')
 
-    lookupBtn.addEventListener('click', lookUpCountry)
+    // Perform normal search if the regular lookup button is clicked
+    lookupBtn.addEventListener('click', function(){
+        lookUpCountryData('normal');
+    });
+
+    // Perform city search if the regular lookup button is clicked
+    cityBtn.addEventListener('click', function(){
+        lookUpCountryData('city');
+    });
 });
 
-async function lookUpCountry(){
+async function lookUpCountryData(searchType){
     let inputField = document.getElementById('country');
     let resultField = document.getElementById('result')
-    let data = await fetchData(inputField.value);
+    let data = await fetchData(inputField.value, searchType);
     resultField.innerHTML = data;
 }
-async function fetchData(formData){
-    const response = await fetch(`world.php?country=${formData}`);
+
+async function fetchData(formData, searchType){
+    let url = ''
+    if (searchType === 'city'){
+        url = `world.php?country=${formData}&context=cities`;
+    }else{
+        url = `world.php?country=${formData}&context=''`;
+    }
+    const response = await fetch(url);
     if(response.ok){
         // Returns the response as a string
         return response.text();
--- world.php
+++ world.php
@@ -1,32 +1,83 @@
 <?php
+/**
+ * Makes queries to SQL based on the query string
+ */
+function getQuery($conn, $country, $cityParam){
+  if($cityParam != "cities"){
+    return $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+  }else{
+    return $conn->query("SELECT cities.name, cities.district,cities.population 
+                         FROM countries LEFT JOIN cities ON countries.code = cities.country_code 
+                         WHERE countries.name LIKE '%$country%'");
+  }
+}
+
+/**
+ * Gets the html tags for the table header
+ */
+function displayHeader($cityParam){
+  $headerStr = '';
+  $headerStr .= '<tr>';
+  $headerStr .= '<th>Name</th>';
+  if($cityParam != "cities"){
+    $headerStr .= '<th>Continent</th>';
+    $headerStr .= '<th>Independence</th>';
+    $headerStr .= '<th>Head of State</th>';
+    
+  }else{
+    $headerStr .= '';
+    $headerStr .= '<th>District</th>';
+    $headerStr .= '<th>Population</th>';
+  }
+  $headerStr .= '</tr>';
+  return $headerStr;
+}
+
+/**
+ * Gets the respective html tags for the table details
+ */
+function retrieveData($cityParam, $dataStr, $row){
+  if($cityParam != "cities"){
+    $dataStr .= "<tr>";
+    $dataStr .= "<td>{$row['name']}</td>";
+    $dataStr .= "<td>{$row['continent']}</td>";
+    $dataStr .= "<td>{$row['independence_year']}</td>";
+    $dataStr .= "<td>{$row['head_of_state']}</td>";
+    $dataStr .= "</tr>";
+  }else{
+    $dataStr .= "<tr>";
+    $dataStr .= "<td>{$row['name']}</td>";
+    $dataStr .= "<td>{$row['district']}</td>";
+    $dataStr .= "<td>{$row['population']}</td>";
+    $dataStr .= "</tr>";
+  }
+  return $dataStr;
+}
+
+// Initialize key variables
 $host = 'localhost';
 $username = 'lab5_user';
 $password = 'password123';
 $dbname = 'world';
 
+// Retrieve query strings
 $country = $_GET['country'];
-
-
+$cityParam = $_GET['context'];
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-$stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
 
+// Make SQL query based on query string
+$stmt = getQuery($conn, $country, $cityParam);
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-
 ?>
+
 <table>
-  <tr>
-    <th>Name</th>
-    <th>Continent</th>
-    <th>Independence</th>
-    <th>Head of State</th>
-  </tr>
-<?php foreach ($results as $row): ?>
-  <tr>
-    <td><?= $row['name'];?></td>
-    <td><?= $row['continent'];?></td>
-    <td><?= $row['independence_year'];?></td>
-    <td><?= $row['head_of_state'];?></td>
-</tr>
+<?php
+$cityStr = ''; 
+echo displayHeader($cityParam);
+foreach ($results as $row):
+  #echo $row['name'];
+  echo retrieveData($cityParam, $cityStr, $row);
+?>
 <?php endforeach; ?>
 </table>
